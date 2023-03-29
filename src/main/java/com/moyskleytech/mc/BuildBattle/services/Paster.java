7e3b9ff13bb2f8f7503cd84095ccd56022f72b2e
Jonathan Boisclair <jonathan.boisclair@uqtr.ca>
2023-03-29 11:47:45 -0400

    using iterator

--- src/main/java/com/moyskleytech/mc/BuildBattle/services/Paster.java
+++ src/main/java/com/moyskleytech/mc/BuildBattle/services/Paster.java
@@ -9,6 +9,8 @@ import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 import com.moyskleytech.mc.BuildBattle.BuildBattle;
@@ -32,7 +34,7 @@ public class Paster extends Service {
     }
 
     public CompletableFuture<Void> paste(Location source, Location destination, int width, int depth, int height) {
-        List<Location> offsets = new ArrayList<>(height * width * depth * 9);
+        List<Location> offsets = new LinkedList<>();
         for (int x = -width; x <= width; x++) {
             for (int y = -height; y <= height; y++) {
                 for (int z = -depth; z <= depth; z++) {
@@ -43,20 +45,20 @@ public class Paster extends Service {
         CompletableFuture<Void> paster = new CompletableFuture<>();
         int blockPerTick = ObsidianConfig.getInstance().paster().blockPerTick();
         BukkitTask task = new BukkitRunnable() {
+            Iterator<Location> iterator = offsets.iterator();
             @Override
             public void run() {
                 for (int i = 0; i < blockPerTick; i++) {
-                    if (offsets.size() == 0)
+                    if (!iterator.hasNext())
                         break;
-                    Location offset = offsets.get(0);
+                    Location offset = iterator.next();
                     Block dBlock = destination.add(offset.getX(), offset.getY(), offset.getZ()).getBlock();
                     Block sBlock = source.add(offset.getX(), offset.getY(), offset.getZ()).getBlock();
                     dBlock.setBlockData(sBlock.getBlockData(), false);
                     if (dBlock.getState() != null)
                         dBlock.getState().update();
-                    offsets.remove(0);
                 }
-                if (offsets.size() == 0) {
+                if (!iterator.hasNext()) {
                     paster.complete(null);
                 }
             }
@@ -67,7 +69,7 @@ public class Paster extends Service {
     }
 
     public CompletableFuture<Void> unpaste(Location destination, int width, int depth, int height) {
-        List<Location> offsets = new ArrayList<>(height * width * depth * 9);
+        List<Location> offsets = new LinkedList<>();
         for (int x = -width; x <= width; x++) {
             for (int y = -height; y <= height; y++) {
                 for (int z = -depth; z <= depth; z++) {
@@ -78,25 +80,25 @@ public class Paster extends Service {
         CompletableFuture<Void> paster = new CompletableFuture<>();
         int blockPerTick = ObsidianConfig.getInstance().paster().blockPerTick();
         BukkitTask task = new BukkitRunnable() {
+            Iterator<Location> iterator = offsets.iterator();
             @Override
             public void run() {
                 for (int i = 0; i < blockPerTick; i++) {
-                    if (offsets.size() == 0)
+                    if (!iterator.hasNext())
                         break;
-                    Location offset = offsets.get(0);
+                    Location offset = iterator.next();
                     Block b = destination.add(offset.getX(), offset.getY(), offset.getZ()).getBlock();
                     b.setType(Material.AIR);
                     if (b.getState() != null)
                         b.getState().update();
                     offsets.remove(0);
                 }
-                if (offsets.size() == 0) {
+                if (!iterator.hasNext()) {
                     paster.complete(null);
                 }
             }
         }.runTaskTimer(BuildBattle.getInstance(), 1, 1);
         tasks.add(task);
         return paster.thenAccept(e -> tasks.remove(task));
-
     }
 }
